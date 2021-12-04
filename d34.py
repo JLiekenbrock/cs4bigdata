8758d187d96436cc670300c18c486a55cf6aa1f4
JLiekenbrock <69990358+JLiekenbrock@users.noreply.github.com>
2021-12-04 14:14:17 +0100

    finish compulsory tasks

--- d34.py
+++ d34.py
@@ -39,12 +39,6 @@ print('''
 ''')
 
 #5
-def generate(_items: [int]) -> [[int]]:
-    res = [[]]
-    for item in _items:
-        new_set = [r+[item] for r in res]
-        res.extend(new_set)
-    return res
     
 #6 
 print('''
